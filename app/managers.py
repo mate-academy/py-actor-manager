cbca1580609802d494ebebedcc41e0b3e3990fc0
Sergii Hrytchyn <ichbinjagerrr@gmail.com>
2025-01-20 17:17:00 +0200

    implement cinema database

---
+++ app/managers.py
@@ -0,0 +1,52 @@
+import os
+import sqlite3
+
+from app.models import Actor
+
+
+class ActorManager:
+    def __init__(self) -> None:
+        db_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "..", "cinema.sqlite"
+        )
+        self._connection = sqlite3.connect(db_path)
+        self.table_name = "actors"
+
+    def create(self, first_name: str, last_name: str) -> None:
+        self._connection.execute(
+            f"INSERT INTO {self.table_name} "
+            f"(first_name, last_name) VALUES (?, ?) ",
+            (first_name, last_name)
+        )
+        self._connection.commit()
+
+    def all(self) -> list:
+        actor_cursor = self._connection.execute(
+            f"SELECt * FROM {self.table_name}"
+        )
+        return [
+            Actor(id=row[1], first_name=row[0], last_name=row[2])
+            for row in actor_cursor
+        ]
+
+    def update(
+            self,
+            id_to_update: int,
+            new_first_name: str,
+            new_last_name: str
+    ) -> None:
+        self._connection.execute(
+            f"UPDATE {self.table_name} "
+            "SET first_name = ?, last_name = ?"
+            "WHERE id = ? ",
+            (new_first_name, new_last_name, id_to_update)
+        )
+        self._connection.commit()
+
+    def delete(self, id_to_delete: int) -> None:
+        self._connection.execute(
+            f"DELETE FROM {self.table_name} WHERE id = ?",
+            (id_to_delete,)
+        )
+        self._connection.commit()
