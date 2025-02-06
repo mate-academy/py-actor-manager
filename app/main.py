8c988897498e0cfbcd54b1b8520c23c7754e847b
unknown <bku089@gmail.com>
2025-02-06 15:36:26 +0200

    test

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
-# from models import Actor
-# from managers import ActorManager
-#
+from models import Actor
+from managers import ActorManager
+
+
 # if __name__ == "__main__":
 #     Actor.objects = ActorManager()
-#
+
 #     Actor.objects.create(first_name="Emma", last_name="Watson")
 #     Actor.objects.create(first_name="Daniel", last_name="Radclife")
 #     print(Actor.objects.all())
@@ -11,3 +12,15 @@
 #     print(Actor.objects.all())
 #     Actor.objects.delete(1)
 #     print(Actor.objects.all())
+
+def main():
+    db_path = "cinema.db"
+    manager = ActorManager(db_path)
+
+    manager.create("John", "Doe")
+    manager.create("Jane", "Smith")
+
+    actors = manager.all()
+    for actor in actors:
+        print(actor)
+    
