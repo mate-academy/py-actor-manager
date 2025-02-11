4aee6666941b6ccdd820fad3b6685fcba72480c1
Mykyta <mitina1900@gmail.com>
2025-02-11 13:40:41 +0200

    Solution

---
+++ app/managers.py
@@ -0,0 +1,53 @@
+import sqlite3
+from app.models import Actor
+
+
+class ActorManager:
+    def __init__(self):
+        self.connection = sqlite3.connect('cinema.sqlite')
+        self.cursor = self.connection.cursor()
+
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS actors ("
+                    "id INTEGER PRIMARY KEY, "
+                    "first_name TEXT NOT NULL, "
+                    "last_name TEXT NOT NULL)"
+        )
+
+
+    def create(self, first_name: str, last_name: str) -> None:
+        self.cursor.execute(
+            "INSERT INTO actors (first_name, last_name)"
+            " VALUES (?, ?)",
+            (first_name, last_name)
+        )
+        self.connection.commit()
+
+
+    def all(self) -> list[Actor]:
+        self.cursor.execute("SELECT * FROM actors")
+        rows = self.cursor.fetchall()
+        return [Actor(id=row[0], first_name=row[1], last_name=row[2]) for row in rows]
+
+
+    def update(self, actor_id: int, first_name: str, last_name: str) -> None:
+        self.cursor.execute(
+            "UPDATE actors"
+            " SET first_name=?, last_name=?"
+            " WHERE id=?",
+            (first_name, last_name, actor_id)
+        )
+        self.connection.commit()
+
+
+    def delete(self, actor_id: int) -> None:
+        self.cursor.execute(
+            "DELETE FROM actors "
+            "WHERE id=?",
+            (actor_id,)
+        )
+        self.connection.commit()
+
+
+    def __del__(self) -> None:
+        self.connection.close()
