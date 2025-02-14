52c85f892263795bc9860ce896c2e60a1e3f08af
Artem <stilon670@gmail.com>
2025-02-14 20:17:41 +0200

    fix flake8

--- app/managers.py
+++ app/managers.py
@@ -4,11 +4,11 @@ from app.models import Actor
 
 
 class ActorManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self.actor_db_connect = sqlite3.connect("../cinema.sqlite")
         self.db_name = "actors"
 
-    def create(self, first_name, last_name):
+    def create(self, first_name: str, last_name: str) -> None:
         self.actor_db_connect.execute(
             f"INSERT INTO {self.db_name} "
             f"(first_name, last_name) VALUES (?, ?)",
@@ -17,7 +17,7 @@ class ActorManager:
 
         self.actor_db_connect.commit()
 
-    def update(self, actor_id, first_name, last_name):
+    def update(self, actor_id: int, first_name: str, last_name: str) -> None:
         self.actor_db_connect.execute(
             f"UPDATE {self.db_name} "
             f"SET first_name = ?, last_name = ?"
@@ -27,14 +27,14 @@ class ActorManager:
 
         self.actor_db_connect.commit()
 
-    def all(self):
+    def all(self) -> list:
         actor_db_cursor = self.actor_db_connect.execute(
             f"SELECT * FROM {self.db_name}"
         )
 
         return [Actor(*actor) for actor in actor_db_cursor]
 
-    def delete(self, actor_id):
+    def delete(self, actor_id: int) -> None:
         self.actor_db_connect.execute(
             f"DELETE FROM {self.db_name} WHERE id = ?",
             (actor_id,)
