ed427ad9b215d17e34f805ffe181eb2eddbca3d9
Ksenia Malashchuk <malashchuk.ksenia@gmail.com>
2025-03-02 21:59:04 +0200

    Solution

--- app/managers.py
+++ app/managers.py
@@ -2,7 +2,6 @@ import sqlite3
 from madels import Actor
 
 
-
 class ActorManager:
     def __init__(self):
         self._connection = sqlite3.connect("cinema.sqlite")
@@ -17,20 +16,36 @@ class ActorManager:
         ]
         return actors
 
-    def create(self, first_name, last_name):
+    def create(self, first_name: str, last_name: str):
         cursor = self._connection.cursor()
-        cursor.execute("INSERT INTO actors (first_name, last_name) VALUES (?, ?)", (first_name, last_name))
+        cursor.execute(
+            "INSERT INTO actors (first_name, last_name) VALUES (?, ?)",
+            (first_name, last_name)
+        )
         self._connection.commit()
         actor_id = cursor.lastrowid
-        return Actor(id=actor_id, first_name=first_name, last_name=last_name)
+        return Actor(
+            id=actor_id,
+            first_name=first_name,
+            last_name=last_name
+        )
 
-    def update(self, actor_id, first_name=None, last_name=None):
+    def update(self, actor_id: int, first_name=None, last_name=None):
         if first_name:
-            self._connection.execute("UPDATE actors SET first_name = ? WHERE id = ?", (first_name, actor_id))
+            self._connection.execute(
+                "UPDATE actors SET first_name = ? WHERE id = ?",
+                (first_name, actor_id)
+            )
         if last_name:
-            self._connection.execute("UPDATE actors SET last_name = ? WHERE id = ?", (last_name, actor_id))
+            self._connection.execute(
+                "UPDATE actors SET last_name = ? WHERE id = ?",
+                (last_name, actor_id)
+            )
         self._connection.commit()
 
     def delete(self, actor_id):
-        self._connection.execute("DELETE FROM actors WHERE id = ?", (actor_id,))
+        self._connection.execute(
+            "DELETE FROM actors WHERE id = ?",
+            (actor_id,)
+        )
         self._connection.commit()
