627e58fca72cd197af9e70a8f3e1dd5face47d4b
Ksenia Malashchuk <malashchuk.ksenia@gmail.com>
2025-03-02 22:12:22 +0200

    Solution

--- app/managers.py
+++ app/managers.py
@@ -10,7 +10,11 @@ class ActorManager:
     def all(self) -> list[Actor]:
         actor_cursor = self._connection.execute("SELECT * FROM actors")
         actors = [
-            Actor(id=row["id"], first_name=row["first_name"], last_name=row["last_name"])
+            Actor(
+                id=row["id"],
+                first_name=row["first_name"],
+                last_name=row["last_name"]
+            )
             for row in actor_cursor.fetchall()
         ]
         return actors
@@ -23,21 +27,30 @@ class ActorManager:
         )
         self._connection.commit()
         actor_id = cursor.lastrowid
-        return Actor(id=actor_id, first_name=first_name, last_name=last_name)
+        return Actor(
+            id=actor_id,
+            first_name=first_name,
+            last_name=last_name
+        )
 
     def update(self, actor_id: int, first_name: str = None, last_name: str = None) -> None:
         if first_name:
             self._connection.execute(
-                "UPDATE actors SET first_name = ? WHERE id = ?", (first_name, actor_id)
+                "UPDATE actors SET first_name = ? WHERE id = ?",
+                (first_name, actor_id)
             )
         if last_name:
             self._connection.execute(
-                "UPDATE actors SET last_name = ? WHERE id = ?", (last_name, actor_id)
+                "UPDATE actors SET last_name = ? WHERE id = ?",
+                (last_name, actor_id)
             )
         self._connection.commit()
 
     def delete(self, actor_id: int) -> None:
-        self._connection.execute("DELETE FROM actors WHERE id = ?", (actor_id,))
+        self._connection.execute(
+            "DELETE FROM actors WHERE id = ?",
+            (actor_id,)
+        )
         self._connection.commit()
 
     def close(self) -> None:
