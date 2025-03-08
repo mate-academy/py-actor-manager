5f52c1820e9935b7c3c9da4dc631c3bca4f84dd1
Vlad <vladlubeka147@gmail.com>
2025-03-08 20:29:19 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,7 @@
-# from models import Actor
-# from managers import ActorManager
-#
-# if __name__ == "__main__":
-#     Actor.objects = ActorManager()
-#
-#     Actor.objects.create(first_name="Emma", last_name="Watson")
-#     Actor.objects.create(first_name="Daniel", last_name="Radclife")
-#     print(Actor.objects.all())
-#     Actor.objects.update(2, "Daniel", "Radcliffe")
-#     print(Actor.objects.all())
-#     Actor.objects.delete(1)
-#     print(Actor.objects.all())
+from dataclasses import dataclass
+
+@dataclass
+class Actor:
+    id: int
+    first_name: str
+    last_name: str
