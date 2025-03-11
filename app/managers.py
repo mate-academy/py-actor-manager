83c00adf6324443eb7622cd8ef09f037c334d480
Olha Yukhnenko <pyolh36@gmail.com>
2025-03-11 13:51:31 +0200

    fix_annotation

--- app/managers.py
+++ app/managers.py
@@ -4,27 +4,34 @@ from models import Actor
 
 
 class ActorManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self._connection = sqlite3.connect("cinema.sqlite")
         self.table_name = "actors"
 
-    def create(self, first_name: str, last_name: str):
+    def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table_name}"
+            f" (first_name, last_name) VALUES (?, ?)",
             (first_name, last_name)
         )
 
         self._connection.commit()
 
-    def all(self):
+    def all(self) -> list:
         cinema_cursor = self._connection.execute(
             f"SELECT * FROM {self.table_name}"
         )
         return [Actor(*row) for row in cinema_cursor]
 
-    def update(self, id_to_update: int, first_name: str, last_name: str):
+    def update(
+            self,
+            id_to_update: int,
+            first_name: str,
+            last_name: str
+    ) -> None:
         self._connection.execute(
-            f"UPDATE {self.table_name} SET first_name=?, last_name=? WHERE id=?",
+            f"UPDATE {self.table_name}"
+            f" SET first_name=?, last_name=? WHERE id=?",
             (first_name, last_name, id_to_update)
         )
         self._connection.commit()
