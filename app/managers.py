80f003016ff0d76769b855f858e5f3a747122aa2
BronkstoneBro <atlantwindow@gmail.com>
2025-03-18 18:16:49 +0200

    fix flake8 mistakes

--- app/managers.py
+++ app/managers.py
@@ -2,9 +2,10 @@ import sqlite3
 
 from models import Actor
 
+
 class ActorManager:
-    def __init__(self):
-        self._connection = sqlite3.connect('../cinema.sqlite')
+    def __init__(self) -> None:
+        self._connection = sqlite3.connect("cinema.sqlite")
         self.table_name = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
@@ -17,24 +18,25 @@ class ActorManager:
         )
         self._connection.commit()
 
-
-    def all(self):
+    def all(self) -> list:
         cursor_connection = self._connection.cursor()
         cursor_connection.execute(
             f"SELECT * FROM {self.table_name}"
         )
         return [Actor(*row) for row in cursor_connection]
 
-    def update(self, id_to_update: int, new_name: str, new_last_name: str):
+    def update(self, id_to_update: int,
+               new_name: str,
+               new_last_name: str) -> None:
         cursor_update = self._connection.cursor()
         cursor_update.execute(
-            f"UPDATE {self.table_name} SET first_name=?, last_name=? WHERE id=?",
+            f"UPDATE {self.table_name} "
+            f"SET first_name=?, last_name=? WHERE id=?",
             (new_name, new_last_name, id_to_update)
         )
         self._connection.commit()
 
-
-    def delete(self, id_to_delete: int):
+    def delete(self, id_to_delete: int) -> None:
         cursor_delete = self._connection.cursor()
         cursor_delete.execute(
             f"DELETE FROM {self.table_name} WHERE id = ?", (id_to_delete,)
--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Actor:
-    def __init__(self, id: int, first_name: str, last_name: str):
+    def __init__(self, id: int, first_name: str, last_name: str) -> None:
         self.id = id
         self.first_name = first_name
         self.last_name = last_name
