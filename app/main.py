b161d869f669bd18231ffc348e0907bb2ec4ec84
Oleksii Kartashov <oleksii.kartashov1@gmail.com>
2025-03-19 18:05:59 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
-# from models import Actor
-# from managers import ActorManager
-#
-# if __name__ == "__main__":
-#     Actor.objects = ActorManager()
-#
-#     Actor.objects.create(first_name="Emma", last_name="Watson")
-#     Actor.objects.create(first_name="Daniel", last_name="Radclife")
-#     print(Actor.objects.all())
-#     Actor.objects.update(2, "Daniel", "Radcliffe")
-#     print(Actor.objects.all())
-#     Actor.objects.delete(1)
-#     print(Actor.objects.all())
+from models import Actor
+
+if __name__ == "__main__":
+    Actor.objects.create(first_name="Emma", last_name="Watson")
+    Actor.objects.create(first_name="Daniel", last_name="Radclife")
+
+    print("All Actors:")
+    actors = Actor.objects.all()
+    for actor in actors:
+        print(f'{actor.id}: {actor.first_name} {actor.last_name}')
+
+    Actor.objects.update(2, "Daniel", "Radcliffe")
+
+    print("\nAfter Update:")
+    actors = Actor.objects.all()
+    for actor in actors:
+        print(f"{actor.id}: {actor.first_name} {actor.last_name}")
+
+    Actor.objects.delete(1)
+
+    print("\nAfter Deletion:")
+    actors = Actor.objects.all()
+    for actor in actors:
+        print(f"{actor.id}: {actor.first_name} {actor.last_name}")
