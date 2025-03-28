c0a85494018f40328a3865742a6d3c5b517dfe58
Ruslan Priadko <r.priadko@ukr.net>
2025-03-28 14:48:01 +0200

    table creation added

--- app/main.py
+++ app/main.py
@@ -1,5 +1,22 @@
+import sqlite3
+
 from models import Actor
 from managers import ActorManager
+
+
+connection = sqlite3.connect("cinema.sqlite")
+cursor = connection.cursor()
+cursor.execute(
+    f"CREATE TABLE actors (id        INTEGER "
+    f"                              PRIMARY KEY "
+    f"                              UNIQUE "
+    f"                              NOT NULL, "
+    f"                      first_name TEXT, "
+    f"                      last_name  TEXT "
+    f");"
+)
+connection.close()
+
 
 if __name__ == "__main__":
     Actor.objects = ActorManager()
