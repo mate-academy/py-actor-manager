95c3c95a8eb562cf1f880432bf7e7945ecb2c42f
Max <smolinskijmaksim1@gmail.com>
2025-03-29 14:57:41 +0100

    Solution3

--- app/main.py
+++ app/main.py
@@ -1,5 +1,23 @@
+import sqlite3
+
 from models import Actor
 from managers import ActorManager
+
+
+connection = sqlite3.connect("cinema.sqlite")
+cursor = connection.cursor()
+cursor.execute(
+    "CREATE TABLE actors (id       INTEGER "
+    "                              PRIMARY KEY "
+    "                              AUTOINCREMENT"
+    "                              UNIQUE "
+    "                              NOT NULL, "
+    "                      first_name TEXT, "
+    "                      last_name  TEXT "
+    ");"
+)
+connection.close()
+
 
 if __name__ == "__main__":
     Actor.objects = ActorManager()
@@ -7,7 +25,9 @@ if __name__ == "__main__":
     Actor.objects.create(first_name="Emma", last_name="Watson")
     Actor.objects.create(first_name="Daniel", last_name="Radclife")
     print(Actor.objects.all())
-    Actor.objects.update(2, "Daniel", "Radcliffe")
+    Actor.objects.update(
+        2, "Daniel",
+        "Radcliffe")
     print(Actor.objects.all())
     Actor.objects.delete(1)
     print(Actor.objects.all())
