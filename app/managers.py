515942bc9874065d2402596779b4a50e7fda679d
ValeriyIvanovich <98410772+ValeriyIvanovich@users.noreply.github.com>
2025-04-01 15:28:42 +0300

    Update managers.py

--- app/managers.py
+++ app/managers.py
@@ -4,22 +4,24 @@ from models import Actor
 
 class ActorManager:
 
-    def __init__(self):
+    def __init__(self) -> None:
         self._connection = sqlite3.connect("cinema.sqlite")
         self.table_name = "actors"
 
-    def create(self, first_name: str, last_name: str):
+    def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table_name} "
+            f"(first_name, last_name) "
+            f"VALUES (?, ?)",
             (first_name, last_name),
         )
         self._connection.commit()
 
-    def all(self):
+    def all(self) -> list:
         actors_cursor = self._connection.execute(f"SELECT * FROM actors")
         return [Actor(*row) for row in actors_cursor]
 
-    def update(self, id_update: int, new_first_name: str, new_last_name: str):
+    def update(self, id_update: int, new_first_name: str, new_last_name: str) -> None:
         self._connection.execute(
             f"UPDATE {self.table_name} SET first_name = ?, last_name = ? "
             "WHERE id = ? ",
