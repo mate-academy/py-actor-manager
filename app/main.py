d0023f32fa5f7e111af39b3de272fcb02bb718e3
ValeriyIvanovich <98410772+ValeriyIvanovich@users.noreply.github.com>
2025-04-01 15:50:24 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ from models import Actor
 from managers import ActorManager
 
 if __name__ == "__main__":
-     Actor.objects = ActorManager()
-     Actor.objects.create(first_name="Emma", last_name="Watson")
-     print(Actor.objects.all())
-     Actor.objects.update(1, "Daniel", "Radclifffe")
-     print(Actor.objects.all())
-     Actor.objects.delete(1)
-     print(Actor.objects.all())
+    Actor.objects = ActorManager()
+    Actor.objects.create(first_name="Emma", last_name="Watson")
+    print(Actor.objects.all())
+    Actor.objects.update(1, "Daniel", "Radclifffe")
+    print(Actor.objects.all())
+    Actor.objects.delete(1)
+    print(Actor.objects.all())
