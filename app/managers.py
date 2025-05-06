6bbd14bcc8052e6ff25a31b3d11523e7eefc95d8
Danylo-D87 <quaestor88@gmail.com>
2025-05-06 15:22:04 +0300

    sol without flake8 problem

--- app/managers.py
+++ app/managers.py
@@ -1,10 +1,9 @@
 import sqlite3
-
+from typing import List
 from app.models import Actor
 
 
 class ActorManager:
-
     def __init__(self, db_name: str, table_name: str) -> None:
         self.db_name = db_name
         self.table_name = table_name
@@ -12,22 +11,25 @@ class ActorManager:
         self.cursor = self.conn.cursor()
 
     def create(self, first_name: str, last_name: str) -> None:
-        self.cursor.execute(f"INSERT INTO {self.table_name} (first_name, last_name) "
-                            f"VALUES (?, ?)", (first_name, last_name))
+        self.cursor.execute(f"INSERT INTO {self.table_name} "
+                            f"(first_name, last_name) VALUES (?, ?)",
+                            (first_name, last_name))
         self.conn.commit()
 
-    def all(self):
+    def all(self) -> List[Actor]:
         self.cursor.execute(f"SELECT * FROM {self.table_name}")
         rows = self.cursor.fetchall()
-        actors = [Actor(id=row[0], first_name=row[1], last_name=row[2]) for row in rows]
+        actors = [Actor(id=row[0], first_name=row[1],
+                        last_name=row[2]) for row in rows]
         return actors
 
-    def update(self, pk: int, new_first_name: str, new_last_name: str):
+    def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
         self.cursor.execute(f"UPDATE {self.table_name} "
                             f"SET first_name = ?, last_name = ? WHERE id = ?",
                             (new_first_name, new_last_name, pk))
         self.conn.commit()
 
-    def delete(self, pk: int):
-        self.cursor.execute(f"DELETE FROM {self.table_name} WHERE id = ?", (pk,))
+    def delete(self, pk: int) -> None:
+        self.cursor.execute(f"DELETE FROM {self.table_name} WHERE id = ?",
+                            (pk,))
         self.conn.commit()
