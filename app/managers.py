48cc7ca65e4fd491b83dbdc888114c0b234fe8a6
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-05-09 21:26:05 +0300

    Update managers.py

--- app/managers.py
+++ app/managers.py
@@ -3,4 +3,39 @@ import sqlite3
 from app.models import Actor
 
 
-# add manager here
+class ActorManager:
+  def __init__(self, db_name: str, table_name: str) -> None:
+    self._connection = sqlite3.connect(db_name)
+    self.table_name = table_name
+
+  def create(self, first_name: str, last_name: str) -> None:
+    self._connection.execute(
+      f"INSERT INTO {self.table_name} "
+      f"(format) VALUES (?) ",
+      (first_name, last_name,)
+    )
+    self._connection.commit()
+
+  def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
+    self._conncetion.execute(
+      f"UPDATE {self.table_name} "
+      f"SET first_name=? AND last_name=? "
+      f"WHERE id=? ",
+      (pk, new_first_name, new_last_name,)
+    )
+    self._connection.commit()
+
+  def delete(self, pk: int) -> None:
+    self._connection.execute(
+      f"DELETE FROM {self.table_name} "
+      f"WHERE id=? "
+      (pk,)
+    )
+    self._connection.commit()
+
+  def all(self) -> list:
+    actor.format.cursor = self._connection.execute(
+      f"SELECT * FROM {self.table_name}"
+    )
+    return [Actor(*actor) for actor in actor.format.cursor]
+    
