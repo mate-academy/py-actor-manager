5419bd5df9d7b8191ccb7f7c7e0fcf2c85b426d0
Michal-Naron <michanaron@gmail.com>
2025-05-12 23:21:31 +0200

    Solution

--- app/managers.py
+++ app/managers.py
@@ -1,40 +1,44 @@
 import sqlite3
 
-from app.models import Actor
-
 
 class ActorManager:
-    def __init__(self, db_name, table_name):
+    def __init__(self, db_name: str, table_name: str) -> None:
         self.table_name = table_name
         self.conn = sqlite3.connect(db_name)
         self.cur = self.conn.cursor()
         is_table = self.cur.execute(
-            f'SELECT name FROM sqlite_master WHERE name = "{self.table_name}"'
+            f"SELECT name FROM sqlite_master "
+            f"WHERE name = '{self.table_name}'"
         )
         if is_table.fetchone() is None:
             self.cur.execute(
-                f'CREATE TABLE {self.table_name}(pk INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT)'
+                f"CREATE TABLE {self.table_name}("
+                "pk INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT)"
             )
 
-    def create(self, first_name, last_name):
+    def create(self, first_name: str, last_name: str) -> None:
         self.conn.execute(
-            f'INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)',
+            f"INSERT INTO {self.table_name} (first_name, last_name) "
+            "VALUES (?, ?)",
             (first_name, last_name)
         )
         self.conn.commit()
 
-    def all(self):
-        return self.conn.execute(f'SELECT * FROM {self.table_name}').fetchall()
+    def all(self) -> list[tuple]:
+        return self.conn.execute(
+            f"SELECT * FROM {self.table_name}"
+        ).fetchall()
 
-    def update(self, pk, new_first_name, new_last_name):
+    def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
         self.conn.execute(
-            f'UPDATE {self.table_name} SET first_name = ?, last_name = ? WHERE pk = ?',
+            f"UPDATE {self.table_name} SET first_name = ?, last_name = ? "
+            "WHERE pk = ?",
             (new_first_name, new_last_name, pk)
         )
         self.conn.commit()
 
-    def delete(self, pk):
+    def delete(self, pk: int) -> None:
         self.conn.execute(
-            f'DELETE FROM {self.table_name} WHERE pk = ?', (pk,)
+            f"DELETE FROM {self.table_name} WHERE pk = ?", (pk,)
         )
         self.conn.commit()
