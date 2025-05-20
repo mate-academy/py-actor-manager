caad8283d2021a0b1ac4e5ea17a213941ea62e28
SamuelSant10 <samuelss.tj@gmail.com>
2025-05-19 20:20:24 -0400

    Update managers.py

--- app/managers.py
+++ app/managers.py
@@ -1,6 +1,47 @@
+# app/managers.py
 import sqlite3
+from typing import List
+from models import Actor
 
-from app.models import Actor
 
+class ActorManager:
+    def __init__(self, db_name: str, table_name: str) -> None:
+        self.db_name = db_name
+        self.table_name = table_name
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
 
+        self.cursor.execute(
+            f"""
+            CREATE TABLE IF NOT EXISTS {self.table_name} (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                first_name TEXT NOT NULL,
+                last_name TEXT NOT NULL
+            )
+            """
+        )
+        self.conn.commit()
+
+        def create(self, first_name: str, last_name: str) -> None:
+        self.cursor.execute(
+            f"INSERT INTO {self.table_name} (first_name, last_name) "
+            "VALUES (?, ?)",
+            (first_name, last_name),
+        )
+        self.conn.commit()
+
+    def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
+        self.cursor.execute(
+            f"UPDATE {self.table_name} SET first_name = ?, last_name = ? "
+            "WHERE id = ?",
+            (new_first_name, new_last_name, pk),
+        )
+        self.conn.commit()
+
+    def delete(self, pk: int) -> None:
+        self.cursor.execute(
+            f"DELETE FROM {self.table_name} WHERE id = ?",
+            (pk,),
+        )
+        self.conn.commit()
 # add manager here
